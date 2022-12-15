90613f80e0317ad7487ae5a852dd4b02359867dd
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-15 14:02:11 -0300

    rendering template

--- urlshort.py
+++ urlshort.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1> Hello World a</h1>'
+    return render_template('index.html', cash='Show me the money!')
 
 
 @app.route('/about')
