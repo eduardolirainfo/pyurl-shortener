3de4e0843a29e53d04196855d043dd8586079732
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-16 17:22:19 -0300

    extended templates

--- pyurl_shortener/blueprints/webui/__init__.py
+++ pyurl_shortener/blueprints/webui/__init__.py
@@ -3,7 +3,15 @@ Returns:
     string: generate a short url for a given url from form input on index.html
 """
 from flask import Blueprint
-from .views import index, about, your_url, redirect_to_url, page_not_found
+from .views import (
+    index,
+    about,
+    your_url,
+    redirect_to_url,
+    page_not_found,
+    session_api,
+    url_api,
+)
 
 bp = Blueprint("webui", __name__, template_folder="templates")
 
@@ -11,6 +19,8 @@ bp.add_url_rule("/", view_func=index)
 bp.add_url_rule("/about/", view_func=about)
 bp.add_url_rule("/your-url/", methods=["GET", "POST"], view_func=your_url)
 bp.add_url_rule("/<string:code>", view_func=redirect_to_url)
+bp.add_url_rule("/api/v1/", view_func=session_api)
+bp.add_url_rule("/api/v1/url/", view_func=url_api)
 
 bp.register_error_handler(404, page_not_found)
 # bp.add_url_rule("/<string:error>/", view_func=page_not_found)
--- pyurl_shortener/blueprints/webui/views.py
+++ pyurl_shortener/blueprints/webui/views.py
@@ -8,15 +8,25 @@ from flask import (
     flash,
     abort,
     session,
-)
+)  # noqa E501
 
 
 def index():
     """return index.html
     Returns:
-        string: return index.html
+        string: return index.html with the codes in the session
     """
-    return render_template("index.html", codes=session.keys())
+    is_exist = exist_path()[1]
+    if not is_exist:
+        session.clear()
+    else:
+        with open(exist_path()[0], encoding="utf-8") as urls_file:
+            urls = json.load(urls_file)
+            urls_file.close()
+            for code in urls.keys():
+                if code not in session.keys():
+                    session[code] = False
+    return render_template("index.html", codes=session.keys())  # noqa E501
 
 
 def about():
@@ -30,7 +40,7 @@ def about():
 def exist_path():
     """check if the path exist
     Returns:
-        string: return the path
+        string: return the path and if the path exist
     """
     file_path = os.path.dirname(__file__)
     file_path = os.path.join(file_path, "instances", "urls.json")
@@ -43,7 +53,7 @@ def your_url():
     """return of the post method with url code
        if the method is not post, return to index.html
     Returns:
-        string: return
+        string: return your_url.html with the code
     """
     if request.method == "POST":
         urls = {}
@@ -53,10 +63,12 @@ def your_url():
         if is_exist:
             with open(file_path, encoding="utf-8") as urls_file:
                 urls = json.load(urls_file)
-
-        if request.form["code"] in urls.keys():
-            flash("Short name has already been taken. Select another name")
-            return redirect(url_for("webui.index"))
+            if request.form["code"] in urls.keys():
+                flash("Short name has already been taken. Select another name")
+                return redirect(url_for("webui.index"))
+        # else:
+        #     session[request.form["code"]] = False
+        #     session.clear()
 
         urls[request.form["code"]] = {"url": request.form["url"]}
         with open(file_path, "w", encoding="utf-8") as url_file:
@@ -71,13 +83,14 @@ def your_url():
 def redirect_to_url(code):
     """redirect to the url
     Returns:
-        string: return redirect to the url
+        string: return redirect to the url or abort 404
     """
     file_path, is_exist = exist_path()
 
     if is_exist:
         with open(file_path, encoding="utf-8") as urls_file:
             urls = json.load(urls_file)
+            urls_file.close()
             if code in urls.keys():
                 if "url" in urls[code].keys():
                     return redirect(urls[code]["url"])
@@ -87,7 +100,40 @@ def redirect_to_url(code):
 def page_not_found(error):
     """return page_not_found.html
     Returns:
-        string: return page_not_found.html
+        string: return page_not_found.html with the code
     """
     code = error.code
     return render_template("page_not_found.html"), code
+
+
+def session_api():
+    """return the session keys
+    Returns:
+        string: return the session keys in json format
+    """
+    return {"code": list(session.keys())}
+
+
+def url_api():
+    """return the urls keys
+    Returns:
+        string: return the urls keys in json format
+    """
+    file_path, is_exist = exist_path()
+    urls = {}
+    if is_exist:
+        with open(file_path, encoding="utf-8") as urls_file:
+            urls = json.load(urls_file)
+            urls_file.close()
+            list_url = []
+            for code in urls.keys():
+                if code in session.keys() and session[code]:
+                    list_url.append(
+                        " { 'code' : "
+                        + f"'{code}'"
+                        + " , 'url' : "
+                        + f" '{urls[code]['url']}'"
+                        + " }"
+                    )
+            return {"url_shortener": list_url}
+    return {"code": []}
